9cbcb095b5e35a18070f644aab7db892e4d82611
augusto <augustosaaude@gmail.com>
2023-01-17 16:32:20 -0300

    feat: removendo os comentários do código

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -37,38 +37,27 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
-
-        // esse código vai permitir que a recycler renderize um layout especifico.
         rvMain.adapter = adapter
 
-        // vai gerenciar o layout e definir o camportamento desse layout
         rvMain.layoutManager = LinearLayoutManager(this)
     }
 
-    // Essa classe vai adaptar os itens da celula que vão ficar hospedados na MainViewHolder
     private inner class MainAdapter(private val mainItems: List<MainItem>): RecyclerView.Adapter<MainViewHolder>() {
-
-        // 1 - Ele é responsável para informar para a recycler view, qual o layout XML da célula especifica. (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
-            // pega a view do main_item
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
         }
 
-        // 2 - Método que vai ser disparado toda vez que houver uma rolagem na tela e for necessário trocar o conteúdo da celula.
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
             val itemCurrent = mainItems[position]
             holder.bind(itemCurrent)
         }
 
-        // 3 - informar quantas células essa listagem vai ter.
         override fun getItemCount(): Int {
             return mainItems.size
         }
-
     }
 
-    // A classe MainViewHolder é a classe da celula em si, é onde vamos conseguir buscar a referência da celula do xml
     private class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         fun bind(item :MainItem) {
             val buttonTest: Button = itemView.findViewById(R.id.btn_item)
