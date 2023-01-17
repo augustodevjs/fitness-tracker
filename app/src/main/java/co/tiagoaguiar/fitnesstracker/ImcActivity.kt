536a59a860b581949ff16cecdd968ebdb9449cef
augusto <augustosaaude@gmail.com>
2023-01-17 16:24:01 -0300

    feat: adicionei dados dinâmicos em listas recicláveis

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,7 +1,9 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -40,6 +42,9 @@ class ImcActivity : AppCompatActivity() {
                 ) { dialog, which -> }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,22 +1,78 @@
 package co.tiagoaguiar.fitnesstracker
 
-import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnImc: LinearLayout
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.ic_baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            ),
+        )
 
-        btnImc.setOnClickListener {
-            val navigateImc = Intent(this, ImcActivity::class.java)
-            startActivity(navigateImc)
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.ic_baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            ),
+        )
+
+        val adapter = MainAdapter(mainItems)
+        rvMain = findViewById(R.id.rv_main)
+
+        // esse código vai permitir que a recycler renderize um layout especifico.
+        rvMain.adapter = adapter
+
+        // vai gerenciar o layout e definir o camportamento desse layout
+        rvMain.layoutManager = LinearLayoutManager(this)
+    }
+
+    // Essa classe vai adaptar os itens da celula que vão ficar hospedados na MainViewHolder
+    private inner class MainAdapter(private val mainItems: List<MainItem>): RecyclerView.Adapter<MainViewHolder>() {
+
+        // 1 - Ele é responsável para informar para a recycler view, qual o layout XML da célula especifica. (item)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            // pega a view do main_item
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
         }
+
+        // 2 - Método que vai ser disparado toda vez que houver uma rolagem na tela e for necessário trocar o conteúdo da celula.
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
+        }
+
+        // 3 - informar quantas células essa listagem vai ter.
+        override fun getItemCount(): Int {
+            return mainItems.size
+        }
+
+    }
+
+    // A classe MainViewHolder é a classe da celula em si, é onde vamos conseguir buscar a referência da celula do xml
+    private class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(item :MainItem) {
+            val buttonTest: Button = itemView.findViewById(R.id.btn_item)
+            buttonTest.setText(item.textStringId)
+         }
     }
 }
